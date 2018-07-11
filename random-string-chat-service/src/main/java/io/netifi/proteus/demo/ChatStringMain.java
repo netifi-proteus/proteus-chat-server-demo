70b20a9cfaf276a43ad4b06bf26cbb51f2c79fbc
Kyle Bahr <kyle@netifi.com>
2018-07-11 16:09:17 -0700

    it works

--- random-string-chat-service/src/main/java/io/netifi/proteus/demo/ChatStringMain.java
+++ random-string-chat-service/src/main/java/io/netifi/proteus/demo/ChatStringMain.java
@@ -1,4 +1,90 @@
 package io.netifi.proteus.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.netflix.spectator.atlas.AtlasConfig;
+import io.micrometer.atlas.AtlasMeterRegistry;
+import io.netifi.proteus.Proteus;
+import io.netifi.proteus.fanout.randomstring.RandomStringGeneratorServer;
+import io.netifi.proteus.demo.chat.ChatServer;
+
+import java.time.Duration;
+import java.util.Optional;
+import java.util.UUID;
+
 public class ChatStringMain {
+    private static final Logger logger = LogManager.getLogger(ChatStringMain.class);
+
+    public static void main(String... args) throws Exception {
+        long accessKey = Long.getLong("ACCESS_KEY", 9007199254740991L);
+        String accessToken = System.getProperty("ACCESS_TOKEN", "kTBDVtfRBO4tHOnZzSyY5ym2kfY=");
+        String host = System.getProperty("BROKER_HOST", "edge.prd.netifi.io");
+        int port = Integer.getInteger("BROKER_PORT", 8001);
+        String randomStringDestination = UUID.randomUUID().toString();
+        String chatDestination = UUID.randomUUID().toString();
+
+        System.out.println("system properties [");
+        System.getProperties()
+                .forEach(
+                        (k, v) -> {
+                            System.out.print(k + ": " + v + ", ");
+                        });
+
+        System.out.println("\n]");
+
+        AtlasMeterRegistry registry =
+                new AtlasMeterRegistry(
+                        new AtlasConfig() {
+                            @Override
+                            public Duration step() {
+                                return Duration.ofSeconds(10);
+                            }
+
+                            @Override
+                            public String get(String k) {
+                                return null;
+                            }
+
+                            @Override
+                            public boolean enabled() {
+                                return false;
+                            }
+                        });
+
+        // Build Netifi Connection
+        Proteus stringServerProteus =
+                Proteus.builder()
+                        .group("fanout.randomStringGenerator") // Group name of service
+                        .destination(randomStringDestination)
+                        .accessKey(accessKey)
+                        .accessToken(accessToken)
+                        .meterRegistry(registry)
+                        .host(host) // Proteus Router Host
+                        .port(port) // Proteus Router Port
+                        .build();
+
+        Proteus chatServerProteus =
+                Proteus.builder()
+                        .group("chat-demo") // Group name of service
+                        .destination(chatDestination)
+                        .accessKey(accessKey)
+                        .accessToken(accessToken)
+                        .meterRegistry(registry)
+                        .host(host) // Proteus Router Host
+                        .port(port) // Proteus Router Port
+                        .build();
+
+        MessageDrivenRandomStringGenerator comboGenerator =
+                new MessageDrivenRandomStringGenerator();
+
+        stringServerProteus.addService(
+                new RandomStringGeneratorServer(comboGenerator, Optional.of(registry)));
+
+
+        chatServerProteus.addService(new ChatServer(comboGenerator, Optional.of(registry)));
+
+        chatServerProteus.onClose().block();
+    }
+
 }
--- random-string-chat-service/src/main/java/io/netifi/proteus/demo/MessageDrivenRandomStringGenerator.java
+++ random-string-chat-service/src/main/java/io/netifi/proteus/demo/MessageDrivenRandomStringGenerator.java
@@ -15,41 +15,46 @@ import org.apache.logging.log4j.Logger;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MessageDrivenRandomStringGenerator implements RandomStringGenerator, Chat {
     private static final Logger logger = LogManager.getLogger(MessageDrivenRandomStringGenerator.class);
 
-    private final Flux<String> _source;
+    private final ArrayBlockingQueue<String> _buffer;
 
-    public MessageDrivenRandomStringGenerator(Flux<String> source) {
-        this._source = source;
+    public MessageDrivenRandomStringGenerator() {
+        this._buffer = new ArrayBlockingQueue<>(1000);
     }
 
     public Flux<RandomStringResponse> generateString(RandomStringRequest message, ByteBuf metadata) {
         int min = message.getMin();
         int max = message.getMax();
 
-        return Flux.<RandomStringResponse>empty();
-//        return Flux.<Integer>generate(
-//                sink -> {
-//                    int size = ThreadLocalRandom.current().nextInt(min, max);
-//                    sink.next(size);
-//                })
-//                .flatMap(
-//                        i -> {
-//                            return randomCharGeneratorClient
-//                                    .generateChar(RandomCharRequest.getDefaultInstance())
-//                                    .limitRequest(i)
-//                                    .reduce(
-//                                            "",
-//                                            (s, randomCharResponse) -> {
-//                                                return s + randomCharResponse.getGenerated();
-//                                            });
-//                        })
-//                .map(s -> RandomStringResponse.newBuilder().setGenerated(s).build())
-//                .doOnError(Throwable::printStackTrace)
-//                .doOnNext(s -> logger.info(s.toString()));
+        return Flux.<Integer>generate(
+                sink -> {
+                    int size = ThreadLocalRandom.current().nextInt(min, max);
+                    sink.next(size);
+                })
+                .map(
+                    i -> {
+                        //Default to size-worth of " " characters
+                        String str = new String(new char[i]).replace("\0", " ");;
+                        try {
+                            str = _buffer.take();
+                            while(str.length() < i){
+                                str = new String(new char[2 * str.length()]).replace("\0", str);
+                            }
+                            str = str.substring(0, i);
+                        } catch (InterruptedException ex){
+                            //Swallow, just move on?
+                        }
+                        System.out.println("Providing string:" + str);
+                        return str;
+                    })
+                .map(s -> RandomStringResponse.newBuilder().setGenerated(s).build())
+                .doOnError(Throwable::printStackTrace)
+                .doOnNext(s -> logger.info(s.toString()));
     }
 
     public Mono<Void> join(JoinEvent evt, ByteBuf metadata){
@@ -57,6 +62,9 @@ public class MessageDrivenRandomStringGenerator implements RandomStringGenerator
     }
 
     public Mono<Void> chat(ChatEvent evt, ByteBuf metadata){
+        String msg = evt.getMessage();
+        System.out.println("Received message:" + msg);
+        this._buffer.offer(evt.getMessage());
         return Mono.empty();
     }
 }
